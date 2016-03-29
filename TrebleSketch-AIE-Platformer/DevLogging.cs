7899548f2580346d81c5c53c011af508bb6bc1dc
ILM126 <ILM126@hotmail.com.au>
2016-03-30 10:06:35 +1030

    Added: Debugging Class to handle debugging shit

---
+++ TrebleSketch-AIE-Platformer/DevLogging.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+using MonoGame.Extended.BitmapFonts;
+using MonoGame.Extended.ViewportAdapters;
+
+namespace TrebleSketch_AIE_Platformer
+{
+    class DevLogging
+    {
+        public BitmapFont DebugFont;
+
+        public void ShowDebug()
+        {
+
+        }
+
+        public void DebugColours()
+        {
+
+        }
+    }
+}

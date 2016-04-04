6f23bee88e8fd3f5b57461e99d8720d48d7d54d4
ILM126 <ILM126@hotmail.com.au>
2016-04-04 10:59:13 +0930

    Added: RenderObjects, my version of GameObjects

---
+++ TrebleSketch-AIE-Platformer/RenderObjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+
+namespace TrebleSketch_AIE_Platformer
+{
+    class RenderObjects
+    {
+    }
+}

8df4cf78b3132e637fb6be41cd4beb85c6ed71d6
ILM126 <ILM126@hotmail.com.au>
2016-04-06 21:55:08 +0930

    Added: LoadScene, to handle the map rendering engine. Possibly from Max.

---
+++ TrebleSketch-AIE-Platformer/Scene/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+
+namespace TrebleSketch_AIE_Platformer
+{
+    class LoadScene : SceneClass
+    {
+        public void Scene01()
+        {
+            
+        }
+    }
+}

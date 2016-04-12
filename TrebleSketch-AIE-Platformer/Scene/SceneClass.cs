cd05dc78d54963e91705dd2554fcce19f743ce24
ILM126 <ILM126@hotmail.com.au>
2016-04-13 00:24:35 +0930

    Added: Namespaces of "Player" and "Scene"

--- TrebleSketch-AIE-Platformer/Scene/SceneClass.cs
+++ TrebleSketch-AIE-Platformer/Scene/SceneClass.cs
@@ -10,6 +10,8 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Storage;
+using TrebleSketch_AIE_Platformer.Player;
+using TrebleSketch_AIE_Platformer.Scene;
 
 namespace TrebleSketch_AIE_Platformer
 {

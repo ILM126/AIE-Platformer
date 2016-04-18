150ba88f5cd7fe5ce323b379fe6936d0d11c4c0e
ILM126 <ilm126@hotmail.com.au>
2016-04-18 19:36:54 +0930

    Added: "Cursor" for cursor movement!

---
+++ TrebleSketch-AIE-Platformer/Cursor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+
+namespace TrebleSketch_AIE_Platformer
+{
+    class Cursor
+    {
+        public Vector2 MousePosition;
+        public MouseState state;
+        public Texture2D MouseTexture;
+
+        public void Update()
+        {
+            state = Mouse.GetState();
+            MousePosition = new Vector2(state.X, state.Y);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                MouseTexture
+                , MousePosition
+                , new Rectangle(0, 0, MouseTexture.Width, MouseTexture.Height)
+                , Color.White);
+        }
+    }
+}

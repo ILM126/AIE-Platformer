6925a8ded709574f2912ab1f3e5c17dd9eda8b12
ILM126 <ILM126@hotmail.com.au>
2016-04-20 21:49:54 +0930

    Just referenced "EclipsingGameUtils" in Cursor, nothing done yet

--- TrebleSketch-AIE-Platformer/Cursor.cs
+++ TrebleSketch-AIE-Platformer/Cursor.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Storage;
+using EclipsingGameUtils;
 
 namespace TrebleSketch_AIE_Platformer
 {

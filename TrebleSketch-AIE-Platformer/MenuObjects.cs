0035f02ce0d8efc92838ddc9db7a6f2d7ce76ebe
ILM126 <ILM126@hotmail.com.au>
2016-04-21 22:49:11 +0930

    (Broken) Added: MenuObjects.cs to help with rendering the menu items, like buttons and what not

---
+++ TrebleSketch-AIE-Platformer/MenuObjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Design;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Storage;
+using EclipsingGameUtils;
+
+namespace TrebleSketch_AIE_Platformer
+{
+    class MenuObjects : SceneObjects
+    {
+    }
+}

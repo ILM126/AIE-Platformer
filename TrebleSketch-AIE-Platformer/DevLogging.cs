10646d66173e6e23b409acb4f7febebe51d29b84
ILM126 <ILM126@hotmail.com.au>
2016-04-26 00:07:40 +0930

    Added: MATA's debugging style, thanks to some SO Questions

--- TrebleSketch-AIE-Platformer/DevLogging.cs
+++ TrebleSketch-AIE-Platformer/DevLogging.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,14 +27,43 @@ namespace TrebleSketch_AIE_Platformer
             Debug.WriteLine("[INFO] LOL WAT MATE!");
         }
 
-        public void InGameDebug()
+        public string GetCurrentDirectory()
+        // http://stackoverflow.com/questions/26432887/c-sharp-access-to-path-denied
+        // http://stackoverflow.com/questions/21726088/how-to-get-current-working-directory-path-c
+        // http://stackoverflow.com/questions/17118537/an-unhandled-exception-of-type-system-unauthorizedaccessexception-occurred-in
+        // And more SO code...
         {
-
+            string currentDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Platformer");
+            string currentFile = Path.ChangeExtension(currentDirectory, ".log");
+            return currentFile;
         }
 
-        public void DebugColours()
+        /// <summary>
+        /// All debugging shall be written to this
+        /// </summary>
+        /// <param name="text"></param>
+        public void WriteToFile(string text, bool toConsole)
         {
-
+            if (toConsole)
+            {
+                Console.WriteLine(text);
+            }
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(GetCurrentDirectory(), true))
+                {
+                    writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt ") + text);
+                    writer.Close();
+                }
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                WriteToFile("[DEBUG] SHIT, not again. Another Unauthorized Access Exception at StreamWriter, I thought I fixed it!", true);
+            }
+            catch (Exception)
+            {
+                WriteToFile("[DEBUG] StreamWriter encountered an exception", true);
+            }
         }
     }
 }

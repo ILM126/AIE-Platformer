336f1f0dce1fbb6ba220eb1b673c77f89af8b225
ILM126 <ilm126@hotmail.com.au>
2016-04-30 16:46:39 +0930

    Removed any useless code in "DevLogging"

--- TrebleSketch-AIE-Platformer/DevLogging.cs
+++ TrebleSketch-AIE-Platformer/DevLogging.cs
@@ -1,19 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Diagnostics;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Design;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Storage;
 using MonoGame.Extended.BitmapFonts;
-using MonoGame.Extended.ViewportAdapters;
 
 namespace TrebleSketch_AIE_Platformer
 {
